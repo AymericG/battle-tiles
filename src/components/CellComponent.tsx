ff408d19a6d5011c77e8ed3ea9824b3ec8ae82f9
Aymeric Gaurat-Apelli <aymeric@gaurat.net>
2024-09-13 17:42:25 +1000

    Stacks

--- src/components/CellComponent.tsx
+++ src/components/CellComponent.tsx
@@ -1,6 +1,7 @@
 import React from 'react';
 import { Cell } from '../models/Cell';
 import { TileComponent } from './TileComponent';
+import './CellComponent.css';
 
 export const CellComponent: React.FC<{ cell: Cell, onTileDrop: (e: React.DragEvent<HTMLDivElement>) => void }> = ({ cell, onTileDrop }) => (
   <div className="cell" onDrop={onTileDrop} onDragOver={(e: React.DragEvent<HTMLDivElement>) => {
